ca0672796045261df3a4fc530848f9af5a31cf9b
Alex Rodriguez <alex.rodriguezsntg@gmail.com>
2021-03-18 18:06:41 -0400

    Done

--- context-review-tests-starter/src/App.js
+++ context-review-tests-starter/src/App.js
@@ -1,9 +1,13 @@
 import coffeeBean from "./mockData/coffeeBeans.json";
+import SelectedCoffeeBean from "./components/SelectedCoffeeBean";
+import SetCoffeeBean from "./components/SetCoffeeBean";
 
 function App() {
   return (
     <>
       <h1>Welcome to Coffee App</h1>
+      <SelectedCoffeeBean />
+      <SetCoffeeBean coffeeBeans={coffeeBean} />
     </>
   );
 }
